2b5f3504f7f9191c570bad63e6a854347f1e4ee2
Jay Park <junho.jay.park@gmail.com>
2022-08-17 20:07:24 -0400

    delete Article function in view.py

--- first_django/view.py
+++ first_django/view.py
@@ -1,28 +1,5 @@
-from django.http import HttpResponse
-from django.template.loader import render_to_string, get_template
-from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
-
-from articles.models import Article
-from experience.models import Experience
-from course.models import Course
-from praises.models import Praise
-
-from .utils import process_modal_vars
-
-import random
-import datetime
 
 
 def home_view(request):
-    numb = random.randint(5, 8)
-    lucky_number = random.randint(1, 2022)
-    welcome = Article.objects.get(id=numb)
-    context = {
-        "lucky_number": lucky_number,
-        "welcome": welcome,
-        "modal": process_modal_vars(
-            "Modal Title", "Do you want to delete?", "YES",
-        )
-    }
-    return render(request, "home-view.html", context=context)
+    return render(request, "home-view.html", context={})
